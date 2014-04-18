282288f3291df8a585fa80fda2e1642e87d90e46
Jose Parrella <j@bureado.com>
2014-04-18 15:47:41 -0430

    Adding Endpoint split guidance

--- devpub/devpub.php
+++ devpub/devpub.php
@@ -1,15 +1,17 @@
 <?php
 	include("proton.php");
 
-	$username  = "vm";
-	$password  = "8vFQaK4gthWf9yoRdjOD2yx7VMmjXBP9uZvTZwKBAHo=";
-	$namespace = "eg-pre-01.servicebus.windows.net";
-	$topic     = "alpha";
+	// Split the connection string manually:
+	// Endpoint=sb://$namespace/;SharedAccessKeyName=$username;SharedAccessKey=$password
+	$username  = "...";
+	$password  = "...";
+	$namespace = "...";
+	$topic     = "..."; // Name of the topic, queue, etc.
 
 	$messenger = new Messenger();
 	$messenger->start();
 
-	while( $f = fgets(STDIN) ){
+	while( $f = fgets(STDIN) ){ // Iterates on STDIN
 		$message = new Message();
 		$message->address = "amqps://$username:$password@$namespace/$topic";
 		$message->body = $f;
